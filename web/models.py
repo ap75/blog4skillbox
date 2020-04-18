000daca2106a7086c2c261f05b1b6ff1b066c424
ap75 <pavel.alexeyev@gmail.com>
2020-04-18 10:57:52 +0300

    Part 1: Add messages form and list on page 'conacts' and store messages to DB

--- web/models.py
+++ web/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class Message(models.Model):
+    name = models.CharField(max_length=120)
+    text = models.TextField()
+
+
 class Publication(models.Model):
     name = models.CharField(max_length=120)
     date = models.DateTimeField(auto_now_add=True)
--- web/views.py
+++ web/views.py
@@ -5,7 +5,7 @@ from django.forms import model_to_dict
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .models import Publication
+from .models import Message, Publication
 
 
 def index(request):
@@ -13,7 +13,31 @@ def index(request):
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    if request.method == 'POST':
+        # Кто-то пытается добавить новое сообщение на страницу контактов
+        # Секретный код проверять не будем - пусть это могут делать все
+        # Проверим, все ли поля заполнены и добавим сообщение в список
+        name = request.POST['name']
+        text = request.POST['text']
+        if not name:
+            return render(request, 'contacts.html', {
+                'error': 'Пустое имя, представьтесь, пожалуйста',
+                'messages': Message.objects.all()
+            })
+        if not text:
+            return render(request, 'contacts.html', {
+                'error': 'Пустой текст, напишите что-нибудь',
+                'messages': Message.objects.all()
+            })
+        Message.objects.create(
+            name=name,
+            text=text.replace('\n', '<br />')
+        )
+    # Независимо от метода (GET или PUT) мы должны отрендерить и показать
+    # страницу с контактами
+    return render(request, 'contacts.html', {
+        'messages': Message.objects.all()
+    })
 
 
 def publish(request):
